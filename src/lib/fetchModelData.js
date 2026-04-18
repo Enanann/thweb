aa4cf53441a5a57b0a288d6c2a16b39ba68d0349
Trần Tiến Dũng <trantiendungxg@gmail.com>
2026-04-18 08:48:55 +0000

    .

--- src/lib/fetchModelData.js
+++ src/lib/fetchModelData.js
@@ -17,20 +17,16 @@ async function fetchModel(url) {
   const backend = `https://3dr9ls-8081.csb.app/api${url}`;
 
   try {
-    // Gọi HTTP GET Request tới Backend
     const response = await fetch(backend);
 
-    // Kiểm tra xem server có trả về lỗi (404, 500...) không
     if (!response.ok) {
       throw new Error(`HTTP Error! Status: ${response.status}`);
     }
 
-    // Chuyển đổi dữ liệu thô nhận được thành object JSON
     const data = await response.json();
     console.log(data);
     return data;
   } catch (error) {
-    // In ra lỗi để dễ debug nếu sập mạng hoặc server chết
     console.error("Lỗi khi fetch từ server:", error);
     throw error;
   }
